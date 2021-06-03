6e7f3ec5fc8822384d7abd1fe7c40660c1e20c00
7204-OMER <Eyupbhr24@gmail.com>
2021-06-03 18:25:52 +0300

    add index & app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import LaptopComp from "./components/LaptopComp";
 import PhoneComp from "./components/PhoneComp";
+import WatchComp from "./components/WatchComp";
+import CompComp from "./components/CompComp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Techno Shop</header>
+      <h1>Clarus Shop</h1>
       <LaptopComp />
       <PhoneComp />
+      <WatchComp />
+      <CompComp />
     </div>
   );
 }
--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,22 +1,27 @@
-import { SELL_LAPTOP, SELL_WATCH, SELL_PHONE, SELL_COMP } from "../types/types";
+import { SELL_LAPTOP, SELL_COMP, SELL_WATCH, SELL_PHONE } from "../types/types";
 
 export const sellLaptop = () => {
   return {
     type: SELL_LAPTOP,
   };
 };
+
 export const sellPhone = () => {
   return {
     type: SELL_PHONE,
   };
 };
-export const sellComp = () => {
-  return {
-    type: SELL_COMP,
-  };
-};
-export const sellWatch = () => {
+
+export const sellWatch = (number = 1) => {
   return {
     type: SELL_WATCH,
+    payload: number,
+  };
+};
+
+export const sellComp = (number = 1) => {
+  return {
+    type: SELL_COMP,
+    payload: number,
   };
 };
