db7249e3f54c8d938c3a296d01f24a870077bb13
7204-OMER <Eyupbhr24@gmail.com>
2021-06-03 16:00:39 +0300

    add action.js

---
+++ src/redux/actions/actions.js
@@ -0,0 +1,22 @@
+import { SELL_LAPTOP, SELL_WATCH, SELL_PHONE, SELL_COMP } from "../types/types";
+
+export const sellLaptop = () => {
+  return {
+    type: SELL_LAPTOP,
+  };
+};
+export const sellPhone = () => {
+  return {
+    type: SELL_PHONE,
+  };
+};
+export const sellComp = () => {
+  return {
+    type: SELL_COMP,
+  };
+};
+export const sellWatch = () => {
+  return {
+    type: SELL_LAPTOP,
+  };
+};
---
+++ src/redux/types/types.js
@@ -0,0 +1,4 @@
+export const SELL_LAPTOP = "SELL-LAPTOP";
+export const SELL_PHONE = "SELL-PHONE";
+export const SELL_WATCH = "SELL-WATCH";
+export const SELL_COMP = "SELL-COMP";
